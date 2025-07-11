578cd4762586c3a40004e6beaa1c6a1feddd3877
Amire Joseph <amirejoseph83@gmail.com>
2025-07-11 09:56:26 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
     development: {
@@ -6,32 +6,32 @@ module.exports = {
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
         host: process.env.DB_HOST,
-        dialect: 'mysql',
+        dialect: "mysql",
         dialectOptions: {
             ssl: {
                 require: true,
                 rejectUnauthorized: false, // Use true if you want to verify the server's SSL certificate
-            }
-        }
+            },
+        },
     },
     test: {
         username: process.env.LOCAL_USER,
         password: process.env.LOCAL_PASSWORD,
-        database: `${process.env.LOCAL_NAME}_test`,
+        database: process.env.LOCAL_NAME,
         host: process.env.LOCAL_HOST,
-        dialect: 'mysql'
+        dialect: "mysql",
     },
     production: {
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
         host: process.env.DB_HOST,
-        dialect: 'mysql',
+        dialect: "mysql",
         dialectOptions: {
             ssl: {
                 require: true,
                 rejectUnauthorized: false, // Use true if you want to verify the server's SSL certificate
-            }
-        }
-    }
-}
+            },
+        },
+    },
+};
